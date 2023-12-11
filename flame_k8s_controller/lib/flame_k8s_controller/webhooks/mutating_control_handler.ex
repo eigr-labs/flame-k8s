7d164eff2c4ff7c2a23ce1e51b211c83d4b0424b
Adriano Santos <sleipnir@bsd.com.br>
2023-12-10 21:44:15 -0300

    Add mutate webhook

--- flame_k8s_controller/lib/flame_k8s_controller/webhooks/mutating_control_handler.ex
+++ flame_k8s_controller/lib/flame_k8s_controller/webhooks/mutating_control_handler.ex
@@ -2,21 +2,50 @@ defmodule FlameK8sController.Webhooks.MutatingControlHandler do
   @moduledoc """
   Mutating Webhook Handler for Postgres Kompo
   """
-  use K8sWebhoox.ResourceConversion.Handler
+  use K8sWebhoox.AdmissionControl.Handler
 
-  def convert(
-        %{"apiVersion" => "apps/v1", "kind" => "Deployment"} = resource,
-        "apps/v1"
-      ) do
-    # TODO inject envs
-    {:ok, put_in(resource, ~w(metadata labels), %{"foo" => "bar"})}
+  alias K8sWebhoox.Conn
+
+  import K8sWebhoox.AdmissionControl.AdmissionReview
+
+  mutate "apps/v1/deployments", conn do
+    %Conn{
+      request: request,
+      response: response
+    } = conn
+
+    %{"operation" => op, "object" => %{"spec" => spec}} = request
+
+    resp =
+      if op == "CREATE" do
+        patch_obj =
+          [
+            %{
+              "op" => "replace",
+              "path" => "/spec/containers/0/env",
+              "value" => %{}
+            }
+          ]
+          |> Jason.encode()
+          |> Base.decode64!()
+
+        %Conn{
+          conn
+          | response: %{response | patch: patch_obj, patchType: "JSONPatch"}
+        }
+      else
+        conn
+      end
+
+    allow(resp)
   end
 
-  def convert(
-        %{"apiVersion" => "apps/v1", "kind" => "StatefulSet"} = resource,
-        "apps/v1"
-      ) do
-    # TODO inject envs
-    {:ok, put_in(resource, ~w(metadata labels), %{"foo" => "bar"})}
+  mutate "apps/v1/statefulsets", conn do
+    %Conn{
+      request: _request,
+      response: _response
+    } = conn
+
+    allow(conn)
   end
 end
